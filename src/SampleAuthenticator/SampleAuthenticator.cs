5d313402378aab55765cdceca61732926866d66d
Abin Abraham Alichan <abinaa@microsoft.com>
2021-02-15 11:20:51 -0800

    TDSProxy, skip client side auth, static server side auth

--- src/SampleAuthenticator/SampleAuthenticator.cs
+++ src/SampleAuthenticator/SampleAuthenticator.cs
@@ -6,20 +6,20 @@ using TDSProxy.Authentication;
 
 namespace SampleAuthenticator
 {
-	[Export(typeof(IAuthenticator))]
-	public class SampleAuthenticator : IAuthenticator
-	{
-		public AuthenticationResult Authenticate(IPAddress clientIp, string username, string password, string database)
-		{
-			return new AuthenticationResult
-			{
-				// ReSharper disable once StringLiteralTypo
-				AllowConnection = !string.Equals(username, "baduser", StringComparison.OrdinalIgnoreCase),
-				ConnectToDatabase = database,
-				ConnectAsUser = username,
-				ConnectUsingPassword = password,
-				DisplayUsername = username
-			};
-		}
-	}
+    [Export(typeof(IAuthenticator))]
+    public class SampleAuthenticator : IAuthenticator
+    {
+        public AuthenticationResult Authenticate(IPAddress clientIp, string username, string password, string database)
+        {
+            return new AuthenticationResult
+            {
+                // ReSharper disable once StringLiteralTypo
+                AllowConnection = true,
+                ConnectToDatabase = database,
+                ConnectAsUser = !string.IsNullOrWhiteSpace(username) ? username : "sa",
+                DisplayUsername = !string.IsNullOrWhiteSpace(username) ? username : "sa",
+                ConnectUsingPassword = !string.IsNullOrWhiteSpace(password) ? password : "YourStrong@Passw0rd",
+            };
+        }
+    }
 }
