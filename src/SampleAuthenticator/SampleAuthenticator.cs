88e5faaf9bc28ca138b6c4c01358decd892a1c8e
Abin Abraham Alichan <abinaa@microsoft.com>
2021-04-05 06:25:32 -0700

    migrate to netcoreapp3.1/netstandard2.0

--- src/SampleAuthenticator/SampleAuthenticator.cs
+++ src/SampleAuthenticator/SampleAuthenticator.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Net;
 using System.ComponentModel.Composition;
+using System.Net;
 
 using TDSProxy.Authentication;
 
 namespace SampleAuthenticator
 {
     [Export(typeof(IAuthenticator))]
-    public class SampleAuthenticator : IAuthenticator
-    {
-        public AuthenticationResult Authenticate(IPAddress clientIp, string username, string password, string database)
-        {
-            return new AuthenticationResult
-            {
-                // ReSharper disable once StringLiteralTypo
-                AllowConnection = true,
-                ConnectToDatabase = database,
-                ConnectAsUser = !string.IsNullOrWhiteSpace(username) ? username : "sa",
-                DisplayUsername = !string.IsNullOrWhiteSpace(username) ? username : "sa",
-                ConnectUsingPassword = !string.IsNullOrWhiteSpace(password) ? password : "YourStrong@Passw0rd",
-            };
-        }
-    }
+	public class SampleAuthenticator : IAuthenticator
+	{
+		public AuthenticationResult Authenticate(IPAddress clientIp, string username, string password, string database)
+		{
+			return new AuthenticationResult
+			{
+				// ReSharper disable once StringLiteralTypo
+				AllowConnection = !string.Equals(username, "baduser", StringComparison.OrdinalIgnoreCase),
+				ConnectToDatabase = database,
+				ConnectAsUser = username,
+				ConnectUsingPassword = password,
+				DisplayUsername = username
+			};
+		}
+	}
 }
--- src/TDSProxy/Program.cs
+++ src/TDSProxy/Program.cs
@@ -1,29 +1,21 @@
 ﻿using System;
-using System.ServiceProcess;
 
 namespace TDSProxy
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		static void Main(string[] args)
-		{
-			Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-			var service = new TDSProxyService();
-			if (Environment.UserInteractive)
-			{
-				service.Start(args);
-				Console.Write("Press ESC to end...");
-				while (Console.ReadKey(false).Key != ConsoleKey.Escape) {}
-				service.Stop();
-			}
-			else
-			{
-				ServiceBase.Run(new TDSProxyService());
-			}
-		}
-	}
+            var service = new TDSProxyService();
+            service.Start(args);
+            Console.Write("Press ESC to end...");
+            while (Console.ReadKey(false).Key != ConsoleKey.Escape) { }
+            service.Stop();
+        }
+    }
 }
